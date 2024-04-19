8fb0b20cc32ce3acb67f943f957b60b58f0392f8
Polina Dineva <PDDineva22@codingburgas.bg>
2024-04-20 02:38:40 +0300

    Initialize the data and make an array parts

--- EduEmpower/EduEmpower/Question.cpp
+++ EduEmpower/EduEmpower/Question.cpp
@@ -41,3 +41,44 @@ std::vector<std::string> split(std::string str, char seperator) {
 	}
 	return strings;
 }
+
+// Function to read questions from a file and return them as a vector
+std::vector<Question> getAllQuestions(std::string testName)
+{
+	std::vector<Question> questions;
+	// Create a file stream object
+	std::fstream dataFile;
+
+	// Open the file for reading
+	dataFile.open(testName, std::ios::in);
+	if (dataFile.is_open()) {
+		// Variable to store each lines of the file
+		std:: string line;
+
+		// Read each line of the file until the end is reached
+		while (getline(dataFile, line)) {
+			// Split the line into substrings using '|' as the separator
+			std::vector<std::string> strings = split(line, '|');
+			if (strings.size() != 6) {
+				return questions;
+			}
+
+			Question question;
+			// Set the question number by converting the string at index 0 to an integer
+			question.questionNumber = stoi(strings[0]);
+			question.question = strings[1];
+			// Add the first answer option to the question object by appending it to the 'answers' vector
+			question.answers.push_back(strings[2]);
+			// Add the second answer option
+			question.answers.push_back(strings[3]);
+			// Add the third answer option
+			question.answers.push_back(strings[4]);
+			// Set the index of the correct answer by converting the string at index 5 to an integer
+			question.rightAnswerIndex = stoi(strings[5]);
+
+			questions.push_back(question);
+		}
+		dataFile.close();
+	}
+	return questions;
+}
